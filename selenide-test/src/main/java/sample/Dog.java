46a7e7161ce8e311c3c26c910a850ac8bf9de502
hiroyasu tokita <tokita.hiroyasu@withone.co.jp>
2018-03-13 11:37:05 +0900

    google formatter

--- selenide-test/src/main/java/sample/Dog.java
+++ selenide-test/src/main/java/sample/Dog.java
@@ -2,33 +2,33 @@ package sample;
 
 public class Dog {
 
-	private String name;
-	private int age;
+  private String name;
+  private int age;
 
-	public Dog() {
+  public Dog() {
 
-	}
+  }
 
-	public Dog(String name, int age) {
-		this.name = name;
-		this.age = age;
-	}
+  public Dog(String name, int age) {
+    this.name = name;
+    this.age = age;
+  }
 
 
-	public void setName(final String _name) {
-		this.name = _name;
+  public void setName(final String _name) {
+    this.name = _name;
 
-	}
+  }
 
-	public String getName() {
-		return name;
-	}
+  public String getName() {
+    return name;
+  }
 
-	public int getAge() {
-		return age;
-	}
+  public int getAge() {
+    return age;
+  }
 
-	public void run() {
-		System.out.println(name + " is running.");
-	}
+  public void run() {
+    System.out.println(name + " is running.");
+  }
 }
--- selenide-test/src/test/java/sample/DogTest.java
+++ selenide-test/src/test/java/sample/DogTest.java
@@ -4,18 +4,18 @@ import junit.framework.TestCase;
 
 public class DogTest extends TestCase {
 
-	public DogTest(String name) {
-		super(name);
-	}
+  public DogTest(String name) {
+    super(name);
+  }
 
-	/**
-	 * getNameメソッドのテスト
-	 *
-	 */
-	public void testGetName() {
-		String name = "pochi";
-		Dog dog = new Dog();
-		dog.setName(name);
-		assertEquals(name, dog.getName());
-	}
+  /**
+   * getNameメソッドのテスト
+   *
+   */
+  public void testGetName() {
+    String name = "pochi";
+    Dog dog = new Dog();
+    dog.setName(name);
+    assertEquals(name, dog.getName());
+  }
 }
